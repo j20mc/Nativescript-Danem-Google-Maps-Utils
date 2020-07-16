327a792e42104ec0ede6634b142ebfebbc8c5b30
J20mc <61017929+j20mc@users.noreply.github.com>
2020-07-16 10:05:02 +0200

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,5 @@
+import { Color } from "tns-core-modules/color";
+
 declare module "nativescript-danem-google-maps-utils" {
 
   import { MapView, Position, Marker } from "nativescript-google-maps-sdk";
@@ -12,9 +14,16 @@ declare module "nativescript-danem-google-maps-utils" {
   export function clearMap(): void;
 
   export interface IHeatmapConfig {
-      provider: any;
-      overlay: any;
+    provider: any;
+    overlay: any;
   }
 
-  export function setupHeatmap(mapView: MapView, positions: Array<Position>, config?: IHeatmapConfig) : IHeatmapConfig;
+  export function setupHeatmap(mapView: MapView, positions: Array<Position>, colors: Array<Color>, startPoints: Array<number>): IHeatmapConfig;
+  
+  export function removeHeatmap(): void;
+
+  export function setRadius() : void;
+
+  export function setOpacity() : void;
+
 }
